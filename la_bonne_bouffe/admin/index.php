2bf2551be0ab21e02a445ec6e37b0440e2a1fbc4
dankdoggo <duhfanofdoge@gmail.com>
2016-11-16 12:32:04 +0100

    Ajout du DOM

---
+++ la_bonne_bouffe/admin/index.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Connexion</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+
+	<!-- Formulaire de connexion -->
+	<div class="col-sm-6 col-sm-push-3">
+		<form method="post">
+			<label for="username">Nom d'utilisateur</label><br>
+			<input type="text" name="username" id="username" class="form-control">
+
+			<br>
+			<label for="password">Mot de passe</label><br>
+			<input type="password" name="password" id="password" class="form-control">
+
+			<br>
+			<input type="submit" value="Se connecter" class="btn btn-primary">
+		</form>
+	</div>
+</body>
+</html>
