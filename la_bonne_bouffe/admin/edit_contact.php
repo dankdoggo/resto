553c852f17bbfe1410552d220b6d7f81157fc149
dankdoggo <duhfanofdoge@gmail.com>
2016-11-16 15:00:39 +0100

    Liste des messages (edit_contact.php)

---
+++ la_bonne_bouffe/admin/edit_contact.php
@@ -0,0 +1,40 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Editer les coordonnées et le slider</title>
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</head>
+<body>
+
+<h1 class="text-center text-info">Listes des messages</h1>
+
+	<!-- Formulaire permettant d'éditer le header -->
+	<div class="col-sm-6 col-sm-push-3">
+		<table class="table">
+			<thead>
+				<tr>
+					<th class="text-center">Prénom</th>
+					<th class="text-center">Nom</th>
+					<th class="text-center">Email</th>
+					<th class="text-center">Message</th>
+					<th class="text-center">Lu</th>
+					<th class="text-center">Supprimer</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td class="text-center "></td>
+					<td class="text-center"></td>
+					<td class="text-center"></td>
+					<td class="text-center"></td>
+					<td class="text-center"><span class="glyphicon glyphicon-ok alert alert-success"></span></td>
+					<td class="text-center"><span class="glyphicon glyphicon-remove alert alert-danger"></span></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</body>
+</html>
