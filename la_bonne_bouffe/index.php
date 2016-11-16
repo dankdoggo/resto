dac8c01f494f512277b6ff7b559ea44e676eb8c3
Carinette33 <gtcarine@yahoo.fr>
2016-11-16 14:51:33 +0100

    index mis à jour carine

---
+++ la_bonne_bouffe/index.php
@@ -0,0 +1,141 @@
+
+<!DOCTYPE html>
+<html lang="fr">
+
+	<head>
+
+		<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="description" content="TEXTE">
+        <meta name="author" content="TEXTE">
+        <meta name="robots" content="index, follow, archive">
+
+        <!--META FB-->
+        <meta property="og:title" content="TEXTE">
+        <meta property="og:description" content="TEXTE">
+        <meta property="og:local" content="fr-FR">
+        <meta property="og:site_name" content="TEXTE">
+        <meta property="og:image" content="chemin/acces/url.jpg">
+        <meta property="og:type" content="website-article">
+
+
+
+		<title> Bienvenue au restaurant la Bonne Bouffe </title>
+
+	 	<!-- Police Google Font -->
+	    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet"> 
+	    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet"> 
+	            
+	     <!--Icone FontAwesome CDN Bootstrape-->
+	    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous"> 
+	         
+	     <!--Feuille de style Bootstrape-->
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+		 <!-- My CSS -->
+        <link href="css/style.css" rel="stylesheet">
+
+	</head>
+
+	<body>
+
+		<div class="wrapper">
+	        
+	        <header id="header">
+
+	        	<div class="bloc-contact">
+	        		<div class="titre-contact"> La bonne bouffe</div>
+	        		<div class="info-contact"> 1 rue de l'avenue - 33000 Bordeaux </div>
+	        		<div class="info-contact"> 01.23.45.67.89</div>
+	        		
+	        	</div>
+
+	      	 	<nav></nav>
+
+	      	 	<div>Nous contacter</div>
+	        	
+
+	        </header>
+	        
+	        <main>
+
+
+	        	<section id="section-slider">
+
+	        		<div id="slider"></div>
+
+	        	</section>
+
+
+	        	<section id="section-recipe">
+
+	        		<h1 class="home h1-home text-center">Les recettes des chefs</h1>
+
+	        		<div class="wrapper-recipe">
+
+		        		<div id="recipe1" class="bloc-recipe">
+		        			<div class="bloc-img-recipe">
+		        				<img class="img-recipe" src="img/burger.jpg"></img>
+		        			</div>
+		        			<h2 class="home h2-home">Lire la recette</h2>
+		        		</div>
+
+						<div id="recipe2" class="bloc-recipe">
+							<div class="bloc-img-recipe">
+		        				<img class="img-recipe" src="img/pate.jpg"></img>
+		        			</div>
+							<h2 class="home h2-home">Lire la recette</h2>
+						</div>
+
+						<div id="recipe2" class="bloc-recipe">
+							<div class="bloc-img-recipe">
+		        				<img class="img-recipe" src="img/samoussa.jpg"></img>
+		        			</div>
+							<h2 class="home h2-home">Lire la recette</h2>
+						</div>
+
+					</div>
+
+	        		
+	        	</section>
+
+				<section id="section-link-recipe">
+
+					<div class="link-recipe center-block"> Découvrir toutes nos recettes de Chef </div>
+					
+				</section>
+
+
+
+	        </main>
+
+	        <footer>
+	        	
+	        </footer>
+
+
+	     </div> <!-- fermeture wrapper  -->  
+        
+        <!-- 
+*******************************************************************************************
+SCRIPTS DE FIN DE PAGE : NE PAS TOUCHER, NE RIEN ECRIRE APRES
+*******************************************************************************************
+-->
+        <!-- jQuery -->
+<!--        <script src="js/jquery.js"></script>-->
+
+        <!-- Bootstrap Core JavaScript -->
+<!--        <script src="js/bootstrap.min.js"></script>-->
+
+        <!-- APPEL Jquery CDN-->
+   <!--     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>-->
+        
+<!--    <script src="js/monscript"> </script>-->
+
+
+    </body>
+
+</html>
