334b9d505c4ac83e1571d4760b803a61a5b13095
JcDecour <doudouc27@gmail.com>
2016-11-16 15:58:43 +0100

    page de confirmatio de suppression pour les recette et l'utilisateur

---
+++ la_bonne_bouffe/admin/delete_recipe.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Supprimer une recette</title>
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+<body>
+
+
+	<main class="container">
+
+		<h1 class="text-center text-info">
+			<i class="fa fa-trash"></i> Supprimer une recette
+		</h1>
+		
+
+			<form method="post">
+				 <input type="button" onclick="history.back();" value="Annuler" class="btn btn-default">
+
+				 <input type="submit" name="delete" value="Oui, je veux supprimer cette merde !" class="btn btn-success">
+			</form>
+
+	</main>
+
+</body>
+</html>
