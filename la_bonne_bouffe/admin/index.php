4db41a6ecccaa15f9b6245c053331a0218930e55
dankdoggo <duhfanofdoge@gmail.com>
2016-11-16 16:32:52 +0100

    blabla

--- la_bonne_bouffe/admin/index.php
+++ la_bonne_bouffe/admin/index.php
@@ -1,4 +1,61 @@
 <?php
+
+session_start();
+
+require_once '../inc/connect.php';
+require_once '../inc/functions.php';
+
+if(!empty($_POST)){
+
+	//Nettoyage du donnée
+	foreach ($_POST as $key => $value) {
+		$post[$key] = trim(strip_tags($value));
+	}
+
+	//Vérification de l'existence du surnom en bdd
+	if(!usernameExist($post['username'], $bdd)){
+		$errors[] = 'Le couple identifiant/mot de passe est incorrect';
+	}
+
+	if(empty($post['password'])){
+		$errors[] = 'Le mot de passe doit être complété';
+	}
+
+	if(count($errors) === 0){
+		$select = $bdd->prepare('SELECT * FROM users WHERE username = :username');
+		$select->bindValue(':username', $post['username']);
+
+		if($select->execute()){
+			$user = $select->fetch(PDO::FETCH_ASSOC); // On récupère l'utilisateur pour pouvoir comparer son mot de passe et le connecter si besoin
+
+			//Si les mots de passe correspondent, on peuple la session
+			if(password_verify($post['password'], $user['password'])){	
+					$formValid = true;
+
+					if($formValid){
+						$_SESSION = [
+							'username'		=>	$user['username'],
+							'permission'	=>	$user['permission'],
+							'email'			=> 	$user['email'],
+							'id'			=> 	$user['id'],
+					];
+						header('Location: ../list_users.php');
+						die();
+					}else{
+						header('Location: index.php');
+					}
+			}
+			else {
+				$errors[] = 'Le couple identifiant/mot de passe est incorrect';
+				$hasError = true;
+			}
+		}
+	}else{
+		$hasError = true;
+	}
+
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,7 +65,11 @@
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
-
+	<?php
+		if($hasError){
+			echo '<p class="">'.implode
+		}
+	?>
 	<!-- Formulaire de connexion -->
 	<div class="col-sm-6 col-sm-push-3">
 		<h1 class="text-center text-info">Se connecter</h1>
